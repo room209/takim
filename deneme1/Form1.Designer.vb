02d2b268163b83b5217c4e67916ae6cdb12a5fe7
Doğan Güneş <dogangunes@trakya.edu.tr>
2017-11-15 23:41:20 +0300

    button eklendi

--- deneme1/Form1.Designer.vb
+++ deneme1/Form1.Designer.vb
@@ -24,6 +24,7 @@ Partial Class Form1
     Private Sub InitializeComponent()
         Me.TextBox2 = New System.Windows.Forms.TextBox()
         Me.MaskedTextBox1 = New System.Windows.Forms.MaskedTextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
         'TextBox2
@@ -40,11 +41,21 @@ Partial Class Form1
         Me.MaskedTextBox1.Size = New System.Drawing.Size(100, 20)
         Me.MaskedTextBox1.TabIndex = 2
         '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(439, 112)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 3
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.ClientSize = New System.Drawing.Size(898, 316)
+        Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.MaskedTextBox1)
         Me.Controls.Add(Me.TextBox2)
         Me.Name = "Form1"
@@ -55,4 +66,5 @@ Partial Class Form1
     End Sub
     Friend WithEvents TextBox2 As TextBox
     Friend WithEvents MaskedTextBox1 As MaskedTextBox
+    Friend WithEvents Button1 As Button
 End Class
